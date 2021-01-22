c63438952a514ca50eee614ad49abd67b607a904
우승윤 <wys@MacBook-Pro-4.local>
2021-01-22 14:50:40 +0900

    Update space code

--- morsecode.py
+++ morsecode.py
@@ -278,10 +278,10 @@ def encoding_sentence(english_sentence):
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
     ans = []
     english_sentence = get_cleaned_english_sentence(english_sentence)
-    for s in english_sentence:
-        if s != ' ':
-            ans.append(encoding_character(s.upper()))
-    result = ' '.join(ans)
+    for word in english_sentence.split():
+        ans.append(' '.join([encoding_character(ch.upper()) for ch in word]))
+
+    result = '  '.join(ans)
 
     return result
     # ==================================
